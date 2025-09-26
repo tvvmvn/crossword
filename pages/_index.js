8ed8c31bf6b2c2762a11347e7390ab613a2c28c8
tvvmvn <tvvmvn@gmail.com>
2025-09-26 20:30:47 +0900

    revalidation added

---
+++ pages/_index.js
@@ -0,0 +1,82 @@
+import { createPuzzle } from "@/lib/service"
+
+export async function getStaticProps() {
+  
+  const puzzle = createPuzzle()
+  console.log(puzzle)
+  
+  return {
+    props: {
+      puzzle: JSON.parse(JSON.stringify(puzzle))
+    }
+  }
+}
+
+const FILTER_MAP = {
+  ACROSS: (caption) => !caption.down,
+  DOWN: (caption) => caption.down
+}
+
+const FILTER_NAMES = Object.keys(FILTER_MAP)
+
+export default function Home({ puzzle }) {
+  
+  console.log(puzzle)
+  const { board, captions } = puzzle;
+
+  return (
+    <div className="px-4">
+      <h1 className="my-4 font-semibold">
+        Crossword Puzzle
+      </h1>
+
+      <table className="w-full table-fixed">
+        <tbody className="border divide-y">
+          {board.map((row, r) => (
+            <tr key={r} className="divide-x grid grid-cols-8">
+              {row.map((col, c) => (
+                <td key={c} className="relative pt-[100%] bg-gray-200">
+                  {col.active && (
+                    <>
+                      {!!col.label && (
+                        <label 
+                          htmlFor={col.id}
+                          className="absolute top-0 left-0 px-1 font-semibold z-10"
+                        >
+                          {col.label}
+                        </label>
+                      )}
+                      <input 
+                        id={col.id}
+                        type="text" 
+                        className="absolute inset-0 bg-white text-center outline-none"
+                        value={col.value}
+                        readOnly
+                      />
+                    </>
+                  )}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      {FILTER_NAMES.map(name => (
+        <section key={name}>
+          <h3 className="my-4 font-semibold">
+            {name}
+          </h3>
+
+          <ul>
+            {captions.filter(FILTER_MAP[name]).map(caption => (
+              <li key={caption.word}>
+                {caption.label}. {caption.content}
+              </li>
+            ))}
+          </ul>
+        </section>
+      ))}
+    </div>
+  )  
+}
--- pages/index.js
+++ pages/index.js
@@ -1,115 +1,54 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Database } from "@sqlitecloud/drivers";
+import { unstable_noStore as noStore } from "next/cache";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+export async function getStaticProps() {
+  
+  let db;
+  let result;
+  let d = new Date();
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+  try {
+    // noStore(); // Prevents Next.js from caching the database request
+    
+    db = new Database(process.env.SQLITECLOUD_URL);
 
-export default function Home() {
+    const id = d.getMinutes() || 60
+    
+    result = await db.sql(`
+      USE DATABASE crossword_db.db;
+      SELECT * FROM words WHERE id=${id}
+    `);
+
+    console.log(result)
+  } catch (ex) {
+    console.error(ex)
+  } finally {
+    db?.close()
+  }
+
+  console.log(new Date().getMinutes())
+
+  return {
+    props: {
+      d: JSON.parse(JSON.stringify(result[0])),
+      clock: new Date().toUTCString(),
+    },
+    revalidate: 60 * 5, // every 5 minutes
+  }
+}
+
+export default function Home({ d, clock }) {
+  console.log(d)
+
   return (
-    <div
-      className={`${geistSans.className} ${geistMono.className} font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
-    >
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              pages/index.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div className="px-4">
+      <h1 className="my-4 font-semibold">
+        {clock} (Server time)
+      </h1>
+
+      <p className="my-4">
+        <b>{d.id}</b> {d.name} {d.meaning}
+      </p>
     </div>
-  );
+  )
 }
