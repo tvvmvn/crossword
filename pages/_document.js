9a9ee3d58b451a3e0da342bc5eec0341d8379137
tvvmvn <tvvmvn@gmail.com>
2025-10-17 16:47:51 +0900

    .

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -7,6 +8,12 @@ export default function Document() {
       <body className="antialiased">
         <Main />
         <NextScript />
+        <script 
+          async 
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1123671203924892"
+          crossorigin="anonymous"
+        >
+        </script>
       </body>
     </Html>
   );
