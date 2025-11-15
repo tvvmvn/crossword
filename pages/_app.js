b4dbaf69938f9af16d82d4baf28832980dd5e28f
tvvmvn <tvvmvn@gmail.com>
2025-11-15 14:40:02 +0900

    .

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,11 @@
 import "@/styles/globals.css";
-import Script from "next/script";
+
 import { Inter, Roboto_Mono, Dongle } from 'next/font/google'
 const roboto_mono = Roboto_Mono({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
   return (
     <div className={roboto_mono.className}>
-      <Script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1123671203924892"
-        crossOrigin="anonymous"
-      />
       <Component {...pageProps} foo="bar" />
     </div>
   )
